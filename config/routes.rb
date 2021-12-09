312e51c72d8423b1c3a224482992837cfc6f0b33
daisyo0090 <daisho0090@gmail.com>
2021-12-09 10:23:01 +0000

    [add]genres/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+      resources :items,:customers,:genres
+  end
+  devise_for :admin, controllers: {
+    sessions:      'admin/sessions',
+    passwords:     'admin/passwords',
+    registrations: 'admin/registrations',
+  }
   devise_for :customers
   root to: 'homes#top'
-  
-  namespace :admin do
-    resources :items,:customers,:genres
-    devise_for :admins
-  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
