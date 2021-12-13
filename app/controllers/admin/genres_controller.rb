db7593b54434ac6d69647dfe8d6539d82b334cdb
daisyo0090 <daisho0090@gmail.com>
2021-12-13 10:42:46 +0000

    [add]genres/edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -10,12 +10,15 @@ class Admin::GenresController < ApplicationController
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 
   def create
     @genre = Genre.new(genre_params)
     @genre.save
-    redirect_to genres_path
+    redirect_to admin_genre_path
   end
 
   private
