c6d66635c0bda4d5ee48e62568ff1c86681dd5e7
daisyo0090 <daisho0090@gmail.com>
2021-12-19 11:59:36 +0000

    [add]devise:customer

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,21 @@ Rails.application.routes.draw do
   namespace :admin do
       resources :items,:customers,:genres
   end
+
+  namespace :public do
+    resources :customers,:items
+  end
+
   devise_for :admin, controllers: {
     sessions:      'admin/sessions',
     passwords:     'admin/passwords',
     registrations: 'admin/registrations',
   }
-  devise_for :customers
+  devise_for :customers, controllers: {
+    sessions:      'public/sessions',
+    passwords:     'public/passwords',
+    registrations: 'public/registrations',
+  }
   root to: 'homes#top'
 
 
